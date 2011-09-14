357f7065fec259c1182489faa42a5e3c5c2eb411
ph.beaudoin <philippe.beaudoin@bull.net>
2011-09-14 08:50:02 +0200

    In each log table created by emaj_create_group() function, add a emaj_inet_addr column, as a complement to the emaj_user column, to register the ip address of the user who has generated the table update.

--- sql/emaj_src.sql
+++ sql/emaj_src.sql
@@ -606,6 +606,8 @@ $_create_tbl$
          || ' ADD COLUMN emaj_txid BIGINT DEFAULT emaj._txid_current()';
     EXECUTE 'ALTER TABLE ' || v_logTableName
          || ' ADD COLUMN emaj_user VARCHAR(32) DEFAULT session_user';
+    EXECUTE 'ALTER TABLE ' || v_logTableName
+         || ' ADD COLUMN emaj_inet_addr INET DEFAULT inet_client_addr()';
 -- alter the sequence associated to the emaj_id column to set the increment to 2 (so that an update operation can safely have its 2 log rows)
     EXECUTE 'ALTER SEQUENCE ' || v_sequenceName || ' INCREMENT 2';
 
