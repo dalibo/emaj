82f3cf24b48e2a1c2a23b2958ba247fc9f01f162
ph.beaudoin <philippe.beaudoin@bull.net>
2012-02-12 19:24:28 +0100

    Add an input parameter to emaj_snap_group() function to specify the options to used in COPY TO statements like CSV, HEADER,...

--- sql/emaj--unpackaged--0.11.0.sql
+++ sql/emaj--unpackaged--0.11.0.sql
@@ -89,7 +89,7 @@ ALTER EXTENSION emaj ADD FUNCTION emaj._rst_group(v_groupName TEXT);
 ALTER EXTENSION emaj ADD FUNCTION emaj.emaj_log_stat_group(v_groupName TEXT, v_firstMark TEXT, v_lastMark TEXT); 
 ALTER EXTENSION emaj ADD FUNCTION emaj.emaj_detailed_log_stat_group(v_groupName TEXT, v_firstMark TEXT, v_lastMark TEXT);
 ALTER EXTENSION emaj ADD FUNCTION emaj.emaj_estimate_rollback_duration(v_groupName TEXT, v_mark TEXT);
-ALTER EXTENSION emaj ADD FUNCTION emaj.emaj_snap_group(v_groupName TEXT, v_dir TEXT); 
+ALTER EXTENSION emaj ADD FUNCTION emaj.emaj_snap_group(v_groupName TEXT, v_dir TEXT, v_copyOptions TEXT); 
 ALTER EXTENSION emaj ADD FUNCTION emaj.emaj_snap_log_group(v_groupName TEXT, v_firstMark TEXT, v_lastMark TEXT, v_dir TEXT, v_copyOptions TEXT);
 ALTER EXTENSION emaj ADD FUNCTION emaj.emaj_generate_sql(v_groupName TEXT, v_firstMark TEXT, v_lastMark TEXT, v_location TEXT);
 
