4cac449bdb515a494ef1b75c0347960ad6e2895d
ph.beaudoin <phb.emaj@free.fr>
2012-11-30 19:21:43 +0100

    create a README file into the tools directory to explain how to customise and use some available development and test tools.

---
+++ tools/copy2Expected.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# E-Maj
+# Prepare expected files for regression tests
+# For each db version, delete existing .out files from 'expected' directory and 
+#                      copy .out files from 'results' to 'expected' directory
+
+EMAJ_HOME="/home/postgres/proj/emaj"
+
+rm $EMAJ_HOME/test/82/expected/*
+cp $EMAJ_HOME/test/82/results/*.out $EMAJ_HOME/test/82/expected/.
+
+rm $EMAJ_HOME/test/83/expected/*
+cp $EMAJ_HOME/test/83/results/*.out $EMAJ_HOME/test/83/expected/.
+
+rm $EMAJ_HOME/test/84/expected/*
+cp $EMAJ_HOME/test/84/results/*.out $EMAJ_HOME/test/84/expected/.
+
+rm $EMAJ_HOME/test/90/expected/*
+cp $EMAJ_HOME/test/90/results/*.out $EMAJ_HOME/test/90/expected/.
+
+rm $EMAJ_HOME/test/91/expected/*
+cp $EMAJ_HOME/test/91/results/*.out $EMAJ_HOME/test/91/expected/.
+
+rm $EMAJ_HOME/test/92/expected/*
+cp $EMAJ_HOME/test/92/results/*.out $EMAJ_HOME/test/92/expected/.
+
+echo ".out files successfully copied from 'results' to 'expected' directories"
+
