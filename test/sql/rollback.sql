604ffb708e0aa38a5c585b13505fce1d3e11cba1
ph.beaudoin <phb.emaj@free.fr>
2013-03-18 11:46:57 +0100

    Add a rlbk_msg column into the emaj_rlbk table to store a final message for the rollback operation (either OK or KO). Store a completion message (similar to the message inserted into the emaj_hist table) when the operation goes well. And trap the errors in all main rollback functions to populate this new column if a problem occurs. (Some additional trapping to add when pg 8.3 will not be supported any more)

--- test/sql/rollback.sql
+++ test/sql/rollback.sql
@@ -419,7 +419,7 @@ rollback;
 -----------------------------
 select rlbk_id, rlbk_groups, rlbk_mark, rlbk_is_logged, rlbk_nb_session, rlbk_nb_table, rlbk_nb_sequence, 
        rlbk_eff_nb_table, rlbk_status, rlbk_begin_hist_id, 
-       case when rlbk_end_datetime is null then 'null' else '[ts]' end as "end_datetime"
+       case when rlbk_end_datetime is null then 'null' else '[ts]' end as "end_datetime", rlbk_msg
   from emaj.emaj_rlbk order by rlbk_id;
 select rlbs_rlbk_id, rlbs_session, 
        case when rlbs_end_datetime is null then 'null' else '[ts]' end as "end_datetime"
