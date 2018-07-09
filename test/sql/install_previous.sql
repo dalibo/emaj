5687970acbdb5479315dd01f69855b5c36577cb4
Philippe Beaudoin <phb.emaj@free.fr>
2018-07-09 16:39:16 +0200

    Prepare a new empty upgrade script and adjust the regression test environment for the new version.

--- test/sql/install_previous.sql
+++ test/sql/install_previous.sql
@@ -15,7 +15,7 @@ SET default_tablespace TO tspemaj;
 ------------------------------------------------------------
 -- emaj installation in its previous version as an extension
 ------------------------------------------------------------
-CREATE EXTENSION emaj VERSION '2.2.3';
+CREATE EXTENSION emaj VERSION '2.3.0';
 
 ------------------------------------------------------------
 -- check installation
