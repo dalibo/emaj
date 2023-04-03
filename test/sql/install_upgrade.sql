d1733b347bdb4f77c0e8f241fa1ca1336b6713da
Philippe Beaudoin <phb.emaj@free.fr>
2023-04-03 12:20:11 +0200

    Prepare a new empty upgrade script and adjust the regression test environment for the next release.

--- test/sql/install_upgrade.sql
+++ test/sql/install_upgrade.sql
@@ -16,7 +16,7 @@ select * from pg_extension_update_paths('emaj') order by 1,2;;
 -----------------------------------------------------------
 -- emaj update to next_version
 -----------------------------------------------------------
-CREATE EXTENSION emaj VERSION '4.1.0' CASCADE;
+CREATE EXTENSION emaj VERSION '4.2.0' CASCADE;
 
 -- check impact in catalog
 select extname, extversion from pg_extension where extname = 'emaj';
