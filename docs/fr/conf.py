ff7b48eafa54b9e9b1f21f848cdaca22d2d84f19
Philippe Beaudoin <phb.emaj@free.fr>
2023-09-20 18:15:45 +0200

    Adjust both fr and en conf.py files to get a proper html theme on the readthedocs server.

--- docs/fr/conf.py
+++ docs/fr/conf.py
@@ -94,6 +94,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 #
 #html_theme = 'alabaster'
 #html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
