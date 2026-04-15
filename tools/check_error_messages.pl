175083604356bfeb6f2dd6a4474f2a271b761d7d
Philippe Beaudoin <phb.emaj@free.fr>
2026-04-15 07:34:25 +0200

    Fix the check_error_messages.pl tool. A warning message was missing in the list of messages known as not present in regression tests outputs.

--- tools/check_error_messages.pl
+++ tools/check_error_messages.pl
@@ -146,6 +146,7 @@ use warnings; use strict;
                             # installation conditions that are not met during the tests
       if ($msgs{$fnctId} ne 'EXCEPTION:E-Maj installation: The current user (%) is not a superuser.'
        && $msgs{$fnctId} ne 'EXCEPTION:E-Maj installation: The current postgres version (%) is too old for this E-Maj version. It should be at least 11.'
+       && $msgs{$fnctId} ne 'WARNING:E-Maj installation: emaj_viewer role is not supported.'
        && $msgs{$fnctId} ne 'WARNING:E-Maj installation: As the max_prepared_transactions parameter value (%) on this cluster is too low, no parallel'
        && $msgs{$fnctId} ne 'WARNING:emaj_gen_sql_dump_changes_group: the shell sed command does not seem to exist.'
                             # internal errors (errors that should never appear and that would be due to coding error)
